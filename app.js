f92fa090dea5945c4b200886efe71515ca41c7d2
vivek4c4 <vivek.ever@gmail.com>
2016-11-09 20:29:11 +0000

    calculate numeric value of a string

---
+++ app.js
@@ -0,0 +1,21 @@
+(function () {
+  angular.module('myFirstApp',[]);
+  angular.controller('myFirstController',function($scope) {
+    $scope.name="";
+    $scope.total=0;
+    $scope.calculateName=function(){
+
+      total=calculateNumericString($scope.name);
+      $scope.total=total;
+    }
+
+    function calculateNumericString(string) {
+      var calculation=0;
+      for (var i = 0; i < string.length; i++) {
+        calculation+=string.charCodeAt(i);
+      }
+      return calculation;
+    }
+
+  })
+})();
